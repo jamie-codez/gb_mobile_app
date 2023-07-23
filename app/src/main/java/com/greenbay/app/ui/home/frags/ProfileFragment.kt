4ebcbac2a664abdbe1a67e380bd402b103b27f43
jamie-codez <cruiseomondi90@gmail.com>
2023-07-23 23:44:08 +0300

    Added profiles

--- app/src/main/java/com/greenbay/app/ui/home/frags/ProfileFragment.kt
+++ app/src/main/java/com/greenbay/app/ui/home/frags/ProfileFragment.kt
@@ -8,12 +8,13 @@ import android.view.ViewGroup
 import com.greenbay.app.R
 import com.greenbay.app.databinding.FragmentLandingBinding
 import com.greenbay.app.databinding.FragmentProfileBinding
+import com.greenbay.app.ui.auth.viewmodels.AuthViewModel
+import com.greenbay.app.ui.home.viewmodels.HomeViewModel
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val viewModel by lazy {
+         AuthViewModel(requireActivity().application)
     }
 
     override fun onCreateView(
@@ -22,6 +23,18 @@ class ProfileFragment : Fragment() {
     ): View {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getUser().observe(viewLifecycleOwner) {
+            binding.profileNameTv.text = it.username
+            binding.profileFirstNameTv.text = it.firstName
+            binding.profileLastNameTv.text = it.lastName
+            binding.profileEmailTv.text = it.email
+            binding.profilePhoneTv.text = it.phone
+            binding.profileIdNoTv.text = it.idNumber
+        }
     }
 
 }
