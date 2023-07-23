f9a67486ee2362099540b3aaa17a5620b51d2743
jamie-codez <cruiseomondi90@gmail.com>
2023-07-23 20:37:22 +0300

    Global variables

--- app/src/main/java/com/greenbay/app/ui/auth/viewmodels/AuthViewModel.kt
+++ app/src/main/java/com/greenbay/app/ui/auth/viewmodels/AuthViewModel.kt
@@ -16,6 +16,7 @@ import com.greenbay.app.network.RetrofitInstance
 import kotlinx.coroutines.launch
 
 class AuthViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository = Repository(RetrofitInstance.getApiService())
     private var loginStatus: MutableLiveData<Boolean> = MutableLiveData(false)
     private var appUser: MutableLiveData<AppUser> = MutableLiveData()
     private var prefs: SharedPreferences = application.getSharedPreferences(
@@ -30,7 +31,6 @@ class AuthViewModel(application: Application) : AndroidViewModel(application) {
 
     fun login(email: String, password: String, activity: Activity): LiveData<Boolean> {
         viewModelScope.launch {
-            val repository = Repository(RetrofitInstance.getApiService())
             val response = repository.login(LoginModel(email, password))
             if (response.status == 200) {
                 loginStatus = MutableLiveData(true)
@@ -63,7 +63,6 @@ class AuthViewModel(application: Application) : AndroidViewModel(application) {
         val accessToken = prefs.getString("accessToken", "")
         if (!accessToken.isNullOrEmpty()) {
             viewModelScope.launch {
-                val repository = Repository(RetrofitInstance.getApiService())
                 val response = repository.getUser()
                 if (response.status == 200) {
                     val user = response.data as AppUser
@@ -99,7 +98,6 @@ class AuthViewModel(application: Application) : AndroidViewModel(application) {
                 activity.getString(R.string.app_name),
                 Context.MODE_PRIVATE
             ).getString("accessToken", "")
-            val repository = Repository(RetrofitInstance.getApiService())
             val response = accessToken?.let { repository.updateUser(it, appUser.id, appUser) }
             updateUser = if (response?.status == 200) {
                 val user = response.data as AppUser
